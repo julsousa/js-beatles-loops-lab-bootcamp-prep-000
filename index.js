fdc8d868d743371e91a48a8fd62b4bda68553156
Julie Sousa <juliesousa@yahoo.com>
2018-03-01 19:39:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function theBeatlesPlay(musicians, instruments){
   var emptyArray = [];
   for (var i = 0; i < musicians.length; i++) {
-    phrases.push(musicians[i] + " plays " + instruments[i]);
+    emptyArray.push(musicians[i] + " plays " + instruments[i]);
   }
-  return phrases;
+  return emptyArray;
 }
 
 
