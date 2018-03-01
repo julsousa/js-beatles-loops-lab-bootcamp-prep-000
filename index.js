d625eae62b7b548836da9cd01965113fcc56e09d
Julie Sousa <juliesousa@yahoo.com>
2018-03-01 19:33:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function iLoveTheBeatles(n) {
   if (n === 7) {
     return "I love the beetles"
   } elseif (n ===17) {
-    array.push(`I am ${i} strange loops.`)
+    return "I love the beetles"
   } else {
     
   }
