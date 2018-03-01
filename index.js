04cdc606bb8b740a159ff08d35325f15faa2f656
Julie Sousa <juliesousa@yahoo.com>
2018-03-01 19:41:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ function theBeatlesPlay(musicians, instruments){
 
 
 function johnLennonFacts(facts) {
-  
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+  ]
+  while facts
 }  
 function iLoveTheBeatles(n) {
   if (n === 7) {
