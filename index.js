a007f950bb3c70bb1b091f5c0b63f6e25a1c5aa5
Julie Sousa <juliesousa@yahoo.com>
2018-03-01 19:36:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(n) {
   if (n === 7) {
     return "I love the beetles"
-  } else if (n ===17) {
+  } else if (n === 17) {
     return "I love the beetles"
   } else {
     return ""
