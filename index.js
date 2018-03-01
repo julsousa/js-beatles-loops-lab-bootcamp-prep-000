7feba625d064e6ba43cdf651a4e23a731f7f72c5
Julie Sousa <juliesousa@yahoo.com>
2018-03-01 19:48:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,23 +8,23 @@ function theBeatlesPlay(musicians, instruments){
 
 
 function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]
- var newFacts = []
- var i = 0
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+ var newFacts = [];
+ var i = 0;
  while (i < facts.length) {
-   newFacts.push(facts[i] + "!!!")
-   i++
+   newFacts.push(facts[i] + "!!!");
+   i++;
   }
-  return newFacts
+  return newFacts;
 } 
 
 
 function iLoveTheBeatles(n) {
   if (n === 7) {
-    return "I love the beetles"
+    return "I love the beetles";
   } else if (n === 17) {
-    return "I love the beetles"
+    return "I love the beetles";
   } else {
-    return ""
+    return "";
   }
 }
