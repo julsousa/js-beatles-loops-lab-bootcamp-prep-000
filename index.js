6e8d054edaefc809531b0c94235ac96d7e90df13
Julie Sousa <juliesousa@yahoo.com>
2018-03-01 19:43:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function johnLennonFacts() {
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
   ]
- var newFacts = [];
+ var newFacts = []
   var i = 0
     while (i < facts.length) {
         newFacts.push(facts[i] + "!!!")
