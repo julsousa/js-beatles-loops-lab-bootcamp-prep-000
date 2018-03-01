1800a0aeec487e179c4fc7eb11e7a260a0c044b0
Julie Sousa <juliesousa@yahoo.com>
2018-03-01 19:39:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function theBeatlesPlay(musicians, instruments){
-  
+  var emptyArray = [];
+  for (var i = 0; i < musicians.length; i++) {
+    phrases.push(musicians[i] + " plays " + instruments[i]);
+    
+  }
+  return phrases;
 }
 function johnLennonFacts(facts) {
   
